90cca5a3eb6d3a5cef6cf76a9ec2acf5eb2ff24c
Kevin Krato <kratok08@gmail.com>
2019-12-14 10:48:45 -0700

    Write models for palette, project, user

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -1,3 +1,7 @@
 class Palette < ApplicationRecord
+  # model associations
   belongs_to :project
+
+  # validations
+  validates_presence_of :title
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ApplicationRecord
+  # model associations
   belongs_to :user
+  has_many :palettes, dependent: :destroy
+
+  # validations
+  validates_presence_of :title, :description
 end
