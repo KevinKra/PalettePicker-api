a41f422bcf8ed8750b12d612b10b97c2187086ee
Kevin Krato <kratok08@gmail.com>
2019-12-14 19:20:24 -0700

    Write create method for PalettesController

--- app/controllers/palettes_controller.rb
+++ app/controllers/palettes_controller.rb
@@ -1,2 +1,17 @@
 class PalettesController < ApplicationController
+  
+  # POST users/:user_id/projects/:project_id/palettes/:id
+  def create
+    @user = User.find(params[:user_id])
+    @project = @user.projects.find_by!(id: params[:project_id])
+    @project.palettes.create!(palette_params)
+    json_response(@project, :created)
+  end
+
+  # DELETE users/:user_id/projects/:project_id/palettes/:id
+
+  private
+  def palette_params
+    params.permit(:title)
+  end
 end
