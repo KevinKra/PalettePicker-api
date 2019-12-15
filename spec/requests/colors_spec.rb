e21e0bbcbda8e77a40e56f4bff180328942c64fd
Kevin Krato <kratok08@gmail.com>
2019-12-15 08:44:31 -0700

    Write colors create method tests

--- spec/requests/colors_spec.rb
+++ spec/requests/colors_spec.rb
@@ -27,6 +27,31 @@ RSpec.describe 'Colors API', type: :request do
     end
   end
 
+  # Test suite for create
+  # POST "/users/:user_id/projects/:project_id/palettes/:palette_id/colors"
+  describe "POST /users/:user_id/projects/:project_id/palettes/:palette_id/colors" do
+    let(:valid_attributes) { { hex_code: "#000000" } }
+    before { post "/users/#{user_id}/projects/#{project_id}/palettes/#{palette_id}/colors", params: valid_attributes } 
+
+    context 'when request attributes are valid' do
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when request attributes are not valid' do
+      before { post "/users/#{user_id}/projects/#{project_id}/palettes/#{palette_id}/colors", params: {} } 
+
+      it 'returns status 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a failure message' do
+        expect(response.body).to match(/Validation failed: Hex code can't be blank/)
+      end
+    end
+  end
+
   # Test suite for destroy
   # DELETE "/users/:user_id/projects/:project_id/palettes/:palette_id/colors/:id"
 end
