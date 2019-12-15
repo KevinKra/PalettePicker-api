39f3a6c57af0623f288e809cfb7924b85216fb7e
Kevin Krato <kratok08@gmail.com>
2019-12-14 18:36:33 -0700

    Write index and create methods for ProjectsController

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,24 @@
 class ProjectsController < ApplicationController
+  before_action :set_projects
+  # GET '/users/:user_id/projects
+  def index 
+    json_response(@user.projects)
+  end
+
+  # POST '/users/:user_id/projects/:id'
+  def create
+    @user.projects.create!(project_params)
+    json_response(@user, :created)
+  end
+
+  # DELETE '/users/:user_id/projects/:id'
+
+  private
+  def project_params
+    params.permit(:title, :description)
+  end
+
+  def set_projects
+    @user = User.find(params[:user_id])
+  end
 end
