9d15453136cc7742e7fa4d63f9518d5a8a79e059
Kevin Krato <kratok08@gmail.com>
2019-12-15 08:43:19 -0700

    Update palette model relation to have_many colors, update colors factory bot to carry relation to palettes

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -1,6 +1,7 @@
 class Palette < ApplicationRecord
   # model associations
   belongs_to :project
+  has_many :colors
 
   # validations
   validates_presence_of :title
--- spec/factories/colors.rb
+++ spec/factories/colors.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :color do
     hex_code { "#" + (Faker::Number.number(digits: 6)).to_s }
+    palette
   end
 end
