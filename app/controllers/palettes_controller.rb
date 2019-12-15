0e4611030833975906478ef7c9e34f7e1df77954
Kevin Krato <kratok08@gmail.com>
2019-12-15 06:43:25 -0700

    Write paletteController delete method

--- app/controllers/palettes_controller.rb
+++ app/controllers/palettes_controller.rb
@@ -1,17 +1,30 @@
 class PalettesController < ApplicationController
-  
-  # POST users/:user_id/projects/:project_id/palettes/:id
+  before_action :set_user_project
+  before_action :set_project_palette, only: [:destroy]
+
+  # POST /users/:user_id/projects/:project_id/palettes/:id
   def create
-    @user = User.find(params[:user_id])
-    @project = @user.projects.find_by!(id: params[:project_id])
     @project.palettes.create!(palette_params)
     json_response(@project, :created)
   end
 
-  # DELETE users/:user_id/projects/:project_id/palettes/:id
+  # DELETE /users/:user_id/projects/:project_id/palettes/:id
+  def destroy
+    @palette.destroy
+  end
+
 
   private
   def palette_params
     params.permit(:title)
+  end
+
+  def set_user_project
+    @user = User.find(params[:user_id])
+    @project = @user.projects.find_by!(id: params[:project_id])
+  end
+
+  def set_project_palette
+    @palette = @project.palettes.find_by!(id: params[:id])
   end
 end
