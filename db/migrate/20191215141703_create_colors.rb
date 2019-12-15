fc069f909d2c674a5832a9550f607ddcadf06a95
Kevin Krato <kratok08@gmail.com>
2019-12-15 07:23:51 -0700

    Create colors migration

--- db/migrate/20191215141703_create_colors.rb
+++ db/migrate/20191215141703_create_colors.rb
@@ -2,7 +2,6 @@ class CreateColors < ActiveRecord::Migration[6.0]
   def change
     create_table :colors do |t|
       t.string :hex_code
-      t.boolean :locked
       t.references :palette, null: false, foreign_key: true
 
       t.timestamps
